8a9de58ba53a2d38f5c658ad50731aa022c525a4
soheba <pathansoheba@gmail.com>
2023-08-24 01:02:47 +0530

    Updated changes

--- technobyte/src/test/java/com/technobyte/app/TechnoByteApplicationTests.java
+++ technobyte/src/test/java/com/technobyte/app/TechnoByteApplicationTests.java
@@ -1,13 +1,9 @@
 package com.technobyte.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class TechnoByteApplicationTests {
 
-/*	@Test
-	void contextLoads() {
-	}*/
+  /*	@Test
+  void contextLoads() {
+  }*/
 
 }
